85494dd1330ab83487f5fb3545c7bc8b18af7235
Muhammad Abdullah <abdullahbinrafique945@gmail.com>
2023-08-18 11:20:27 +0500

    making a separate reusable component

---
+++ 06-pop-corn/src/StarRating.js
@@ -0,0 +1,29 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "cneter",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+// for using default value in props destructuring we use this: maxRating=5 is default value for this
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>X</p>
+    </div>
+  );
+}
