2bc91348127bed883b3415f78a06e712a74d20a4
Muhammad Abdullah <abdullahbinrafique945@gmail.com>
2023-08-21 12:17:43 +0500

    making more dynamic like API

--- 06-pop-corn/src/index.js
+++ 06-pop-corn/src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="magenta" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie has {movieRating} star ratings!</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible 😑", "Bad 😐", "Okay 🙂", "Good 🥰", "Amazing! 😍"]}
+    />
+    <StarRating color="green" size={42} className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
